820d3d89ab036f35fdc853fe1f373378c7d57277
Sebastian Pina Otey <spinaotey@gmail.com>
2016-12-20 15:19:46 +0100

    Crosspoint function of polygon edge

--- DNAPolygon.c
+++ DNAPolygon.c
@@ -11,3 +11,9 @@ void minMaxInt(int *v, int n, int *min, int *max){
             *max = v[i];
 }
 
+float crosspoint(int x1, int y1, int x2, int y2, int y){
+    float m,n;
+    m = (float)(x1-x2)/(float)(y1-y2);
+    n = (float)x1-(float)y1*m;
+    return((float)y*m+n);   
+}
