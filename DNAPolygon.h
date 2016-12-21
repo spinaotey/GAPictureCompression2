eac694ec9373e7f2386bdd93f8c5248aeecf2b4e
Sebastian Pina Otey <spinaotey@gmail.com>
2016-12-21 10:22:57 +0100

    Change to char the RGBA of polygons

--- DNAPolygon.h
+++ DNAPolygon.h
@@ -1,6 +1,6 @@
 typedef struct tri{
     int px[3],py[3];    //Coordinates of vertices
-    int rgba[4];        //Color RGBA
+    char rgba[4];        //Color RGBA
     int nFill;          //Number of points inside polygon  
     int *xFill, *yFill; //Coordinates for filled points
 }Triangle_t;
