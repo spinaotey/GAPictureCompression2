5d337583b995d6bafef32883a574ed88b2e8401d
Sebastian Pina Otey <spinaotey@gmail.com>
2016-12-23 18:15:41 +0100

    Comments added

--- DNAPicture.h
+++ DNAPicture.h
@@ -2,21 +2,21 @@
 #include "DNAPolygon.h"
 
 typedef struct PicGen_s{
-    unsigned char *r,*g,*b;
-    int width, height;
-    unsigned char bgrgb[3];
-    int npoly;
-    struct Triangle_s *poly;
-    char flag;
-    long fitness;
+    unsigned char *r,*g,*b; //RGB of the picture
+    int width, height;      //Width and height
+    unsigned char bgrgb[3]; //background RGB
+    int npoly;              //number of polygons
+    struct Triangle_s *poly;//Vector of polygons
+    char flag;              //flag to see if fitness was computed
+    long fitness;           //fitness
 }PicGen_t;
 
 typedef struct Picprop_s{
-    unsigned char *r,*g,*b;
-    int width,height;
-    double sdCoords,sdColor;
-    double bd;
-    unsigned char tb;
+    unsigned char *r,*g,*b; //RGB of picture
+    int width,height;       //Width and height
+    double sdCoords,sdColor;//Standard deviation of coordinates and color mutations
+    double bd;              //bound differential
+    unsigned char tb;       //transparency bound
 }Picprop_t;
 
 
